3822c5d7859e99fe7222f23c0caede17d47ed7ae
Kulachenkov <nik.k2710@gmail.com>
2022-03-15 11:12:39 +0300

    Initial commit

---
+++ src/main/java/com/example/mycoursework2chapter/MyCourseWork2chapterApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycoursework2chapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCourseWork2chapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCourseWork2chapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mycoursework2chapter/MyCourseWork2chapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycoursework2chapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCourseWork2chapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
